bb5015030169bbd7407ee1accabda0c298c5af0c
Stepan <stepa.davydov.2002@mail.ru>
2022-12-28 22:26:21 +0300

    Saving urls history and added mobile support

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <StrictMode>
+  // <StrictMode>
+  <>
     <ColorModeScript initialColorMode="dark"/>
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
-  </StrictMode>
+    </>
+  // </StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
